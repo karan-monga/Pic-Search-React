a39541be6b1cc8d46ef1932485ed75d413d3e48c
karan-monga <karanm777@gmail.com>
2020-01-04 17:41:32 -0800

    Updates

---
+++ precache-manifest.4abeb9e01ee25b37f94ef74168d9263e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8412c6b0f3c63b729d8a",
+    "url": "/karan-monga/Pic-Search-React/static/css/main.4f191842.chunk.css"
+  },
+  {
+    "revision": "8412c6b0f3c63b729d8a",
+    "url": "/karan-monga/Pic-Search-React/static/js/main.8412c6b0.chunk.js"
+  },
+  {
+    "revision": "945513008b33705d0de2",
+    "url": "/karan-monga/Pic-Search-React/static/js/1.94551300.chunk.js"
+  },
+  {
+    "revision": "6205ce3e9b800aef2864",
+    "url": "/karan-monga/Pic-Search-React/static/js/runtime~main.6205ce3e.js"
+  },
+  {
+    "revision": "36f78679c91599d82d8a5a9f1e939178",
+    "url": "/karan-monga/Pic-Search-React/index.html"
+  }
+];
